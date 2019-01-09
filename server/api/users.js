4352ae4b8f46ae332637853210933a2f31def01b
Scott DeMeo <sd3605@nyu.edu>
2019-01-09 17:06:48 -0500

    created api endpoint /users/:id/cart to retrieve all items in cart for logged in user

--- server/api/users.js
+++ server/api/users.js
@@ -1,3 +1,25 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const {User, Order, OrderProduct, Product} = require('../db/models')
 module.exports = router
+
+router.get('/:id/cart', async (req, res, next) => {
+  try {
+    if (req.user) {
+      const orderId = await Order.findOne({
+        where: {
+          userId: req.params.id,
+          isBought: false
+        }
+      })
+      const cart = await OrderProduct.findAll({
+        where: {orderId},
+        include: [{model: Product}]
+      })
+      res.json(cart)
+    } else {
+      res.sendStatus(401)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
