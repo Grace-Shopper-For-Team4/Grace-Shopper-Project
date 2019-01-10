e63cb765edf67ec87061a51b5d27f03dae2788d2
Scott DeMeo <sd3605@nyu.edu>
2019-01-10 12:16:56 -0500

    created api POST endpoint api/users/:id/cart to add products to cart

--- server/api/users.js
+++ server/api/users.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 const {User, Order, OrderProduct, Product} = require('../db/models')
 module.exports = router
 
+// get all items in cart
+
 router.get('/:id/cart', async (req, res, next) => {
   try {
     if (req.user && req.user.dataValues.id === Number(req.params.id)) {
@@ -16,6 +18,40 @@ router.get('/:id/cart', async (req, res, next) => {
         include: [{model: Product}]
       })
       res.json(cart)
+    } else {
+      res.sendStatus(401)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+// add item to cart
+
+router.post('/:id/cart', async (req, res, next) => {
+  try {
+    if (req.user && req.user.dataValues.id === Number(req.params.id)) {
+      const {productId, quantity} = req.body
+      const order = await Order.findOne({
+        where: {
+          userId: req.params.id,
+          isBought: false
+        }
+      })
+      const productExistsInCart = await OrderProduct.find({
+        where: {productId}
+      })
+      // only allow user to add item to cart if it doesn't exist in cart already
+      if (!productExistsInCart) {
+        const product = await OrderProduct.create({
+          productId,
+          quantity,
+          orderId: order.dataValues.id
+        })
+        res.status(201).json(product)
+      } else {
+        res.send(409, 'product exists in cart already')
+      }
     } else {
       res.sendStatus(401)
     }
